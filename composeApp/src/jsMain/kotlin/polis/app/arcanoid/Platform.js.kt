44a39f1e53b7a3e40704e298fc996025ba6548c6
Vitalii Polishchuk <vitaliy.polishchuk@gmail.com>
2026-02-21 19:42:56 +0100

    Fixed issue with js platform.

--- composeApp/src/jsMain/kotlin/polis/app/arcanoid/Platform.js.kt
+++ composeApp/src/jsMain/kotlin/polis/app/arcanoid/Platform.js.kt
@@ -1,7 +1,20 @@
 package polis.app.arcanoid
 
+import kotlinx.browser.window
+import kotlin.js.Date
+
 class JsPlatform: Platform {
     override val name: String = "Web with Kotlin/JS"
 }
 
 actual fun getPlatform(): Platform = JsPlatform()
+
+actual fun currentTimeMillis(): Long = Date.now().toLong()
+
+actual fun saveHighScore(score: Int) {
+    window.localStorage.setItem("high_score", score.toString())
+}
+
+actual fun getHighScore(): Int {
+    return window.localStorage.getItem("high_score")?.toIntOrNull() ?: 0
+}
